adfacd41474e1f9fc89880ae4093087fbd82c83b
someone <someone@someplace.com>
2022-10-28 09:08:46 +0530

    port changed

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.on('connection', socket => {
 
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
